6d554d049d621902a150b3cc7f21f0678e27764f
MisterSmooth <seanbrettsmith@gmail.com>
2021-06-30 17:25:48 -0500

    commit to main

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+//const bodyParser = require('body-parser');
+
+
+
+
+//init express so I can use
+
+const app = express();
+const port = 3000;
+
+
+
+app.listen(port);
